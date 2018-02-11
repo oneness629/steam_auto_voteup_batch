abd856414831beed203a6b982370405771e9d633
oneness629 <oneness629@hotmail.com>
2018-02-11 11:08:50 +0800

    去除无用的等待代码，系统退出修正，添加异常捕获代码

--- python/auto_voteup/steamAutoVoteup.py
+++ python/auto_voteup/steamAutoVoteup.py
@@ -23,15 +23,14 @@ browser = Browser()
 
 # 登录用户检查
 def check_login_user():
-
     functions = {
         # 写入存在的登录cookie
         'write_login_cookie_to_browser_content': write_login_cookie_to_browser_content,
-        # 检查登录
+        # 检查用户是否登录
         'check_steam_user_is_login':check_steam_user_is_login
     }
     result_dict = browser.open_browser(get_check_is_login_url('oneness629'), functions, browser.driver, False, False)
-    logging.info('检查用户登录结果 : ' + json.dumps(result_dict).decode("unicode-escape"))
+    logging.info('写入cookie结果和用户是否登录结果 : ' + json.dumps(result_dict).decode("unicode-escape"))
     if result_dict['check_steam_user_is_login'] == False:
         # 执行用户登录
         functions = {'login_from':login_from}
@@ -59,16 +58,6 @@ def voteup():
     result_dict = browser.open_browser(None, functions, browser.driver, False, False)
     logging.info('执行点赞js结果 : ' + json.dumps(result_dict).decode("unicode-escape"))
 
-    # 等待
-    wait(browser.driver)
-    logging.info('等待直到浏览器崩溃或异常等其它问题后重新执行')
-    pass
-
-
-# 等待10秒
-def wait(driver):
-    logging.info('等待10s')
-    time.sleep(10)
 
 
 # 自动点赞
@@ -82,9 +71,31 @@ def auto_voteup():
         # browser.close_browser()
         pass
 
+# 点赞次数
+voteup_count = 99999999
+# 点赞间隔时间(秒)
+voteup_timeout = 60
 
 def main():
-    auto_voteup()
+    try:
+        for i in range(voteup_count):
+            logging.info('第' + str(i+1) + '次点赞操作开始')
+            try:
+                auto_voteup()
+            except BaseException as e:
+                logging.exception(e)
+            logging.info('第' + str(i+1) + '次点赞操作结束')
+            logging.info('等待' + str(voteup_timeout) + '秒后继续操作')
+            time.sleep(voteup_timeout)
+        # 程序正常退出
+        return 0
+    except BaseException as e:
+        logging.exception(e)
+        # 程序异常退出
+        return 1
+    finally:
+        # 最后要关闭浏览器
+        browser.close_browser()
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
