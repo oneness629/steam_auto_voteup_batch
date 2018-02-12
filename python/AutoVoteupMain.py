cb5178f16d6fd31c01308248d37ddd6ed9c1594e
oneness629 <oneness629@hotmail.com>
2018-02-12 14:26:19 +0800

    添加日志分级

--- python/AutoVoteupMain.py
+++ python/AutoVoteupMain.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from auto_voteup import steamAutoVoteup
 import sys
+import logging.config
+from auto_voteup import steamAutoVoteup
+
+logging.config.fileConfig('logging.conf')
 
 if __name__ == '__main__':
     sys.exit(steamAutoVoteup.main())
