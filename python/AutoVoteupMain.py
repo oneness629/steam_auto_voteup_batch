77374833a2abead099f277789797377277488cfc
oneness629 <oneness629@hotmail.com>
2018-02-12 16:57:43 +0800

    日志修正

--- python/AutoVoteupMain.py
+++ python/AutoVoteupMain.py
@@ -2,9 +2,9 @@
 import json
 import sys
 import logging.config
-from auto_voteup import steamAutoVoteup
-
 logging.config.dictConfig(json.load(open('logging.json','r')))
+
+from auto_voteup import steamAutoVoteup
 
 if __name__ == '__main__':
     sys.exit(steamAutoVoteup.main())
